f17cac1d7f00d37bbb9630d79523f31ff4056344
konrad-te <konrad.tyburcy@gmail.com>
2026-02-10 17:02:32 +0100

    Change logic to fetch data

---
+++ backend/tests/main_test2.py
@@ -0,0 +1,140 @@
+import requests
+import json
+from dotenv import load_dotenv
+import os
+import time
+
+load_dotenv()
+
+api_key = os.getenv("airly_api")
+api_key_2 = os.getenv("owm_api")
+
+headers = {
+    'Accept': 'application/json',
+    'apikey': api_key
+}
+
+def find_nearest_station(lat: float, lon: float, max_distance_km: int) -> dict | None:
+    try:
+        url = (
+            f"https://airapi.airly.eu/v2/installations/nearest?lat={lat}&lng={lon}&maxDistanceKM={max_distance_km}"
+              )
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        stations = response.json()
+        if not stations:
+            return None
+        # Airly returns nearest installations first
+        station = stations[0]
+        return {
+            "id": station["id"],
+            "latitude": station["location"]["latitude"],
+            "longitude": station["location"]["longitude"],
+        }
+    except requests.exceptions.HTTPError as e:
+        if e.response.status_code == 401:
+            print(f"Incorrect API key: {e}")
+    except requests.exceptions.RequestException as e:
+        print(f"Airly request failed: {e}")
+    return None
+# Example of the output {'id': 2464, 'latitude': 50.508196, 'longitude': 19.414377}
+
+
+# x = find_nearest_station(50.5082, 19.4148, 5)
+# print(x)
+
+# lon = 
+# lat = 
+# 50.50910227348566, 19.412999254704324
+
+def fetch_air_quality_data(lat: float, lon: float):
+    url = f"https://airapi.airly.eu/v2/measurements/point?lat={lat}&lng={lon}"
+    request_air_quality_data = requests.get(url, headers=headers)
+    request_air_quality_data.raise_for_status()
+    air_quality_data = request_air_quality_data.json()
+    x = json.dumps(air_quality_data, indent = 2)
+    return x
+
+fetch = fetch_air_quality_data(57.77194936436369, 14.15443446524945)
+print(fetch)
+
+
+
+
+
+def fetch_air_quality_data(station_id:int) -> dict:
+    if station_id is not int:
+        print("Incorrect API key")
+    url = f"https://airapi.airly.eu/v2/measurements/installation?installationId={station_id}"
+    request_air_quality_data = requests.get(url, headers=headers)
+    request_air_quality_data.raise_for_status()
+    air_quality_data = request_air_quality_data.json()
+    return air_quality_data
+
+old_data_limit = 3600 # seconds
+
+def get_air_quality_data(station_id:int):
+    filename = f"air_data_{station_id}"
+    if os.path.exists(filename):
+        file_age = time.time() - os.path.getmtime(filename)
+        if file_age < old_data_limit:
+            print("Using fetched data")
+            with open(filename, "r") as f:
+                return json.load(f)
+        else:
+            print("Data is too old, fetching new one.")
+    else:
+        print("No file found. Fetching new data")
+    data = fetch_air_quality_data(station_id)
+    with open(filename, "w") as f:
+        json.dump(data, f, indent=2)
+    return data
+
+def get_pm25_value(meterological_data) -> float:
+    for value in meterological_data.values():
+        for item in value["values"]:
+            if item["name"] == "PM25":
+                return item["value"]
+            
+def translate_pm25(pm25_result: float) -> str:
+    if pm25_result >= 75:
+        return "Extremely Poor"
+    elif pm25_result >= 50:
+        return "Very Poor"
+    elif pm25_result >= 25:
+        return "Poor"
+    elif pm25_result >= 20:
+        return "Medium"
+    elif pm25_result >= 10:
+        return "Good"
+    elif pm25_result >= 0:
+        return "Very Good"
+    else:
+        return "Incorrect pm25 level"
+
+
+def get_temperature(meterological_data) -> float:
+    for value in meterological_data.values():
+        for item in value["values"]:
+            if item["name"] == "TEMPERATURE":
+                return item["value"]
+
+# nearest_station_id = find_nearest_station_id(50.5082, 19.4148, 5)
+# meterological_data = get_air_quality_data(nearest_station_id)
+# pm25_value = get_pm25_value(meterological_data)
+# air_quality = translate_pm25(pm25_value)
+# temp = get_temperature(meterological_data)
+# print(air_quality)
+# print(temp)            
+
+
+# pm25_result = get_pm25_value(meterological_data)
+# print(pm25_result)
+# temp_result = get_temperature(meterological_data)
+# print(temp_result)
+# print(translate_pm25(pm25_result))
+
+
+# print(result)
+
+#def measure_data 
